232924c0b554d96f8e246e9ac3b20fb6383d2871
sindhuparkavi <sindhuparkavi88@gmail.com>
2018-02-08 05:18:58 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -1,7 +1,7 @@
 var index = require("./index.js")
 console.log(index.name)
 console.log(index.height)
-if (index.name === "Susan") {
+if ("index.name" === "Susan") {
     console.log("The name is correct")
 } else {
     console.log("Expected: Susan, Received: "+index.name)
